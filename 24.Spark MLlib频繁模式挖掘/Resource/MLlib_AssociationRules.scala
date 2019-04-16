72230f9a42df2e71608e802a7ba4dbf530c51fe3
yanke312090307 <312090307@qq.com>
2019-04-16 11:04:20 +0800

    Create MLlib_AssociationRules.scala

---
+++ 24.Spark MLlib频繁模式挖掘/Resource/MLlib_AssociationRules.scala
@@ -0,0 +1,37 @@
+package com.xunfang.spark.SparkMLLib.FrequentPatternMining
+
+import org.apache.spark.mllib.fpm.AssociationRules
+import org.apache.spark.mllib.fpm.FPGrowth.FreqItemset
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by hjr on 17-7-18.
+  */
+object MLlib_AssociationRules {
+  def main(args: Array[String]): Unit = {
+    /**
+      * 初始化
+      */
+    val conf = new SparkConf().setMaster("local[4]").setAppName("MLlib_AssociationRules")
+    val sc = new SparkContext(conf)
+
+    val freqItemsets = sc.parallelize(Seq(
+      new FreqItemset(Array("a"), 15L),
+      new FreqItemset(Array("b"), 35L),
+      new FreqItemset(Array("a", "b"), 12L)
+    ))
+
+    val ar = new AssociationRules()
+      .setMinConfidence(0.8)
+    val results = ar.run(freqItemsets)
+
+    results.collect().foreach { rule =>
+      println("[" + rule.antecedent.mkString(",")
+        + "=>"
+        + rule.consequent.mkString(",") + "]," + rule.confidence)
+    }
+
+    sc.stop()
+
+  }
+}
